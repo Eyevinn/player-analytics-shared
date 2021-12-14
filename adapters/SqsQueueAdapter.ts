4f4658e4a7546a2020dabac4db8f09b64dce9680
Nicholas Frederiksen <nikko.frederiksen@gmail.com>
2021-12-14 16:23:44 +0100

    chore: added read and remove function for sqs

--- adapters/SqsQueueAdapter.ts
+++ adapters/SqsQueueAdapter.ts
@@ -2,9 +2,14 @@ import {
   SQSClient,
   SendMessageCommand,
   SendMessageCommandInput,
-} from '@aws-sdk/client-sqs';
-import { AbstractQueueAdapter } from '../types/interfaces';
-import winston from 'winston';
+  ReceiveMessageCommandInput,
+  ReceiveMessageCommand,
+  DeleteMessageCommandInput,
+  Message,
+  DeleteMessageCommand,
+} from "@aws-sdk/client-sqs";
+import { AbstractQueueAdapter } from "../types/interfaces";
+import winston from "winston";
 
 export class SqsQueueAdapter implements AbstractQueueAdapter {
   logger: winston.Logger;
@@ -16,22 +21,22 @@ export class SqsQueueAdapter implements AbstractQueueAdapter {
   }
 
   async pushToQueue(event: Object): Promise<any> {
-    if (process.env.SQS_QUEUE_URL === 'undefined') {
-      return { message: 'SQS_QUEUE_URL is undefined' };
+    if (process.env.SQS_QUEUE_URL === "undefined") {
+      return { message: "SQS_QUEUE_URL is undefined" };
     }
-    if (process.env.AWS_REGION === 'undefined') {
-      return { message: 'AWS_REGION is undefined' };
+    if (process.env.AWS_REGION === "undefined") {
+      return { message: "AWS_REGION is undefined" };
     }
     const params: SendMessageCommandInput = {
       MessageAttributes: {
         Event: {
-          DataType: 'String',
-          StringValue: event['event'],
+          DataType: "String",
+          StringValue: event["event"],
         },
         Time: {
-          DataType: 'String',
-          StringValue: event['timestamp']
-            ? event['timestamp']
+          DataType: "String",
+          StringValue: event["timestamp"]
+            ? event["timestamp"]
             : new Date().toISOString(),
         },
       },
@@ -41,8 +46,58 @@ export class SqsQueueAdapter implements AbstractQueueAdapter {
     const sendMessageCommand = new SendMessageCommand(params);
     try {
       const sendMessageResult = await this.client.send(sendMessageCommand);
-      this.logger.info(`Response from SQS: ${JSON.stringify(sendMessageResult)}`);
+      this.logger.info(
+        `Response from SQS: ${JSON.stringify(sendMessageResult)}`
+      );
       return sendMessageResult;
+    } catch (err) {
+      this.logger.error(JSON.stringify(err));
+      return err;
+    }
+  }
+
+  async readFromQueue() {
+    if (process.env.SQS_QUEUE_URL === "undefined") {
+      return { message: "SQS_QUEUE_URL is undefined" };
+    }
+    if (process.env.AWS_REGION === "undefined") {
+      return { message: "AWS_REGION is undefined" };
+    }
+    const params: ReceiveMessageCommandInput = {
+      QueueUrl: process.env.SQS_QUEUE_URL,
+    };
+    const recieveMessageCommand = new ReceiveMessageCommand(params);
+    try {
+      const recieveMessageResult = await this.client.send(
+        recieveMessageCommand
+      );
+      this.logger.info(
+        `Reserved Messages From SQS Count: ${recieveMessageResult.Messages?.length}`
+      );
+      return recieveMessageResult;
+    } catch (err) {
+      this.logger.error(JSON.stringify(err));
+      return err;
+    }
+  }
+  async removeFromQueue(queueMsg: Message) {
+    if (process.env.SQS_QUEUE_URL === "undefined") {
+      return { message: "SQS_QUEUE_URL is undefined" };
+    }
+    if (process.env.AWS_REGION === "undefined") {
+      return { message: "AWS_REGION is undefined" };
+    }
+    const params: DeleteMessageCommandInput = {
+      QueueUrl: process.env.SQS_QUEUE_URL,
+      ReceiptHandle: queueMsg.ReceiptHandle,
+    };
+    const deleteMessageCommand = new DeleteMessageCommand(params);
+    try {
+      const deleteMessageResult = await this.client.send(deleteMessageCommand);
+      this.logger.info(
+        `Response from SQS: ${JSON.stringify(deleteMessageResult)}`
+      );
+      return deleteMessageResult;
     } catch (err) {
       this.logger.error(JSON.stringify(err));
       return err;
--- spec/adapters/SqsQueueAdapter.spec.ts
+++ spec/adapters/SqsQueueAdapter.spec.ts
@@ -1,74 +1,151 @@
-import { SQSClient } from '@aws-sdk/client-sqs';
-import { SqsQueueAdapter } from '../..';
-import Logger from '../../util/logger';
+import {
+  DeleteMessageCommand,
+  Message,
+  ReceiveMessageCommand,
+  SendMessageCommand,
+  SQSClient,
+} from "@aws-sdk/client-sqs";
+import { mockClient } from "aws-sdk-client-mock";
+import { SqsQueueAdapter } from "../..";
+import Logger from "../../util/logger";
 
-describe('SQS Queue Adapter', () => {
+const sqsMock = mockClient(SQSClient);
+
+describe("SQS Queue Adapter", () => {
   beforeEach(() => {
-    process.env.AWS_REGION = 'us-east-1';
-    process.env.SQS_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/1234/test-queue';
+    process.env.AWS_REGION = "us-east-1";
+    process.env.QUEUE_TYPE = "SQS";
+    process.env.SQS_QUEUE_URL =
+      "https://sqs.us-east-1.amazonaws.com/1234/test-queue";
+    sqsMock.reset();
   });
 
-  it('should not push to queue if sqs queue env is not set', async () => {
+  it("should not push to queue if sqs queue env is not set", async () => {
     process.env.SQS_QUEUE_URL = undefined;
-    spyOn(SQSClient.prototype, 'send').and.callFake(function () {
-      return Promise.resolve({
-        message: 'SQS_QUEUE_URL is undefined',
-      });
-    });
     const queueAdapter = new SqsQueueAdapter(Logger);
     const mockEvent = {
-      event: 'loading',
+      event: "loading",
       timestamp: 0,
       playhead: 0,
       duration: 0,
     };
     let result = await queueAdapter.pushToQueue(mockEvent);
-    expect(result).toEqual({ message: 'SQS_QUEUE_URL is undefined' });
+    expect(result).toEqual({ message: "SQS_QUEUE_URL is undefined" });
   });
-
-  it('should not push to queue if AWS region env is not set', async () => {
+  it("should not read from queue if sqs queue env is not set", async () => {
+    process.env.SQS_QUEUE_URL = undefined;
+    const queueAdapter = new SqsQueueAdapter(Logger);
+    let result = await queueAdapter.readFromQueue();
+    expect(result).toEqual({ message: "SQS_QUEUE_URL is undefined" });
+  });
+  it("should not remove from queue if sqs queue env is not set", async () => {
+    process.env.SQS_QUEUE_URL = undefined;
+    const queueAdapter = new SqsQueueAdapter(Logger);
+    const mockSQSMessage: Message = {
+      MessageId: "62686810-05ba-4b43-62730ff3156g7jd3",
+      ReceiptHandle:
+        "MbZj6wDWli+JvwwJaBV+3dcjk2YW2vA3" +
+        "+STFFljTM8tJJg6HRG6PYSasuWXPJB+C" +
+        "wLj1FjgXUv1uSj1gUPAWV66FU/WeR4mq" +
+        "2OKpEGYWbnLmpRCJVAyeMjeU5ZBdtcQ+" +
+        "QEauMZc8ZRv37sIW2iJKq3M9MFx1YvV11A2x/KSbkJ0=",
+      MD5OfBody: "fafb00f5732ab283681e124bf8747ed1",
+      Body: JSON.stringify({
+        event: "loading",
+        timestamp: 0,
+        playhead: 0,
+        duration: 0,
+      }),
+    };
+    let result = await queueAdapter.removeFromQueue(mockSQSMessage);
+    expect(result).toEqual({ message: "SQS_QUEUE_URL is undefined" });
+  });
+  it("should not push to queue if AWS region env is not set", async () => {
     process.env.AWS_REGION = undefined;
-    spyOn(SQSClient.prototype, 'send').and.callFake(function () {
-      return Promise.resolve({
-        message: 'AWS_REGION is undefined',
-      });
-    });
     const queueAdapter = new SqsQueueAdapter(Logger);
     const mockEvent = {
-      event: 'loading',
+      event: "loading",
       timestamp: 0,
       playhead: 0,
       duration: 0,
     };
     let result = await queueAdapter.pushToQueue(mockEvent);
-    expect(result).toEqual({ message: 'AWS_REGION is undefined' });
+    expect(result).toEqual({ message: "AWS_REGION is undefined" });
+  });
+  it("should not read from queue if AWS region env is not set", async () => {
+    process.env.AWS_REGION = undefined;
+    const queueAdapter = new SqsQueueAdapter(Logger);
+    let result = await queueAdapter.readFromQueue();
+    expect(result).toEqual({ message: "AWS_REGION is undefined" });
+  });
+  it("should not remove from queue if AWS region env is not set", async () => {
+    process.env.AWS_REGION = undefined;
+    const queueAdapter = new SqsQueueAdapter(Logger);
+    const mockSQSMessage: Message = {
+      MessageId: "62686810-05ba-4b43-62730ff3156g7jd3",
+      ReceiptHandle:
+        "MbZj6wDWli+JvwwJaBV+3dcjk2YW2vA3" +
+        "+STFFljTM8tJJg6HRG6PYSasuWXPJB+C" +
+        "wLj1FjgXUv1uSj1gUPAWV66FU/WeR4mq" +
+        "2OKpEGYWbnLmpRCJVAyeMjeU5ZBdtcQ+" +
+        "QEauMZc8ZRv37sIW2iJKq3M9MFx1YvV11A2x/KSbkJ0=",
+      MD5OfBody: "fafb00f5732ab283681e124bf8747ed1",
+      Body: JSON.stringify({
+        event: "loading",
+        timestamp: 0,
+        playhead: 0,
+        duration: 0,
+      }),
+    };
+    let result = await queueAdapter.removeFromQueue(mockSQSMessage);
+    expect(result).toEqual({ message: "AWS_REGION is undefined" });
   });
 
-  it('should catch error when sending message fails', async () => {
+  it("should catch error when sending message fails", async () => {
     const errMsg = {
-      Type: 'Sender',
-      Code: 'AWS.SimpleQueueService.NonExistentQueue',
-      Detail: '',
-      name: 'AWS.SimpleQueueService.NonExistentQueue',
-      $fault: 'client',
+      Type: "Sender",
+      Code: "AWS.SimpleQueueService.NonExistentQueue",
+      Detail: "",
+      name: "AWS.SimpleQueueService.NonExistentQueue",
+      $fault: "client",
       $metadata: {
         httpStatusCode: 400,
-        requestId: 'df840ab9-e68b-5c0e-b4a0-5094f2dfaee8',
+        requestId: "df840ab9-e68b-5c0e-b4a0-5094f2dfaee8",
         attempts: 1,
         totalRetryDelay: 0,
       },
     };
+    const mockSQSMessage: Message = {
+      MessageId: "62686810-05ba-4b43-62730ff3156g7jd3",
+      ReceiptHandle:
+        "MbZj6wDWli+JvwwJaBV+3dcjk2YW2vA3" +
+        "+STFFljTM8tJJg6HRG6PYSasuWXPJB+C" +
+        "wLj1FjgXUv1uSj1gUPAWV66FU/WeR4mq" +
+        "2OKpEGYWbnLmpRCJVAyeMjeU5ZBdtcQ+" +
+        "QEauMZc8ZRv37sIW2iJKq3M9MFx1YvV11A2x/KSbkJ0=",
+      MD5OfBody: "fafb00f5732ab283681e124bf8747ed1",
+      Body: JSON.stringify({
+        event: "loading",
+        timestamp: 0,
+        playhead: 0,
+        duration: 0,
+      }),
+    };
     const mockEvent = {
-      event: 'loading',
+      event: "loading",
       timestamp: 0,
       playhead: 0,
       duration: 0,
     };
-    spyOn(SQSClient.prototype, 'send').and.callFake(function () {
-      return Promise.reject(errMsg);
-    });
+    sqsMock.on(SendMessageCommand).resolves(errMsg);
+    sqsMock.on(ReceiveMessageCommand).resolves(errMsg);
+    sqsMock.on(DeleteMessageCommand).resolves(errMsg);
     const queueAdapter = new SqsQueueAdapter(Logger);
-    let result = await queueAdapter.pushToQueue(mockEvent);
-    expect(result).toEqual(errMsg);
+    let pushResult = await queueAdapter.pushToQueue(mockEvent);
+    let readResult = await queueAdapter.readFromQueue();
+    let removeResult = await queueAdapter.removeFromQueue(mockSQSMessage);
+    expect(pushResult).toEqual(errMsg);
+    expect(readResult).toEqual(errMsg);
+    expect(removeResult).toEqual(errMsg);
   });
 });
