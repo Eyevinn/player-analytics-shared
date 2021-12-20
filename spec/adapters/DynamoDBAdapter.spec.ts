ed2fdcd6dd58ce6120f3ea5581252b9731b3fd01
Nicholas Frederiksen <nikko.frederiksen@gmail.com>
2021-12-20 11:05:13 +0100

    chore: added db adapters with unit tests (#10)
    * chore: added db adapters and started on specs
    
    * chore: update specs, and name changes
    
    * chore: remove unused vars in specs
    
    * chore: fix typo
    
    * chore: add enumerator for handleErrorOutput
    
    * chore: change to upper case
    
    * chore: new folders, update index.ts, fix type def

---
+++ spec/adapters/DynamoDBAdapter.spec.ts
@@ -0,0 +1,173 @@
+import {
+  CreateTableCommand,
+  ListTablesCommand,
+  PutItemCommand,
+  DynamoDBClient,
+  GetItemCommand,
+  DeleteItemCommand,
+  AttributeValue,
+  CreateTableCommandOutput,
+  PutItemCommandOutput,
+  GetItemCommandOutput,
+  DeleteItemCommandOutput,
+} from '@aws-sdk/client-dynamodb';
+import { AwsError, mockClient } from 'aws-sdk-client-mock';
+import { DynamoDBAdapter } from '../../adapters/db/DynamoDBAdapter';
+import { ErrorType } from '../../types/interfaces';
+import Logger from '../../util/logger';
+
+const ddbMock = mockClient(DynamoDBClient);
+
+describe('Dynamo DB Adapter', () => {
+  beforeEach(() => {
+    process.env.AWS_REGION = 'us-east-1';
+    ddbMock.reset();
+  });
+
+  it('should return list of table names in database', async () => {
+    const mockTables = ['table_1', 'table_2', 'table_3'];
+    const DDBReply = {
+      MessageId: '12345678-4444-5555-6666-111122223333',
+      TableNames: mockTables,
+    };
+    const adapter = new DynamoDBAdapter(Logger);
+    ddbMock.on(ListTablesCommand).resolves(DDBReply);
+    const result = await adapter.getTableNames();
+    expect(result).toEqual(mockTables);
+  });
+
+  it('should create a table in database', async () => {
+    const tableName = 'table_1';
+    const DDBReply: CreateTableCommandOutput = {
+      $metadata: {},
+      TableDescription: {
+        TableName: tableName,
+      },
+    };
+
+    const adapter = new DynamoDBAdapter(Logger);
+    ddbMock.on(CreateTableCommand).resolves(DDBReply);
+    const result = await adapter.createTable(tableName);
+    expect(result).toEqual(DDBReply);
+  });
+
+  it('should put item to database', async () => {
+    const DDBReply: PutItemCommandOutput = {
+      $metadata: {},
+    };
+    const adapter = new DynamoDBAdapter(Logger);
+    const mockEvent = {
+      event: 'loading',
+      timestamp: 0,
+      playhead: 0,
+      duration: 0,
+      host: 'mock.tenant.mock',
+    };
+    ddbMock.on(PutItemCommand).resolves(DDBReply);
+    const result = await adapter.putItem({
+      tableName: 'table_1',
+      data: mockEvent,
+    });
+    expect(result).toEqual({ $metadata: {} });
+  });
+
+  it('should label errorType with "continue" when allowed error occurs', async () => {
+    const DDBReply: AwsError = {
+      Type: 'Sender',
+      Code: 'ResourceNotFoundException',
+      name: 'ResourceNotFoundException',
+      $fault: 'client',
+      $metadata: {
+        httpStatusCode: 400,
+        requestId: 'df840ab9-e68b-5c0e-b4a0-5094f2dfaee8',
+        attempts: 1,
+        totalRetryDelay: 0,
+      },
+    };
+    const adapter = new DynamoDBAdapter(Logger);
+    const mockEvent = {
+      event: 'loading',
+      timestamp: 0,
+      playhead: 0,
+      duration: 0,
+      host: 'mock.tenant.mock',
+    };
+    ddbMock.on(PutItemCommand).rejects(DDBReply);
+    try {
+      const result = await adapter.putItem({
+        tableName: 'table_1',
+        data: mockEvent,
+      });
+    } catch (err) {
+      expect(err.errorType).toEqual(ErrorType.CONTINUE);
+      expect(err.error.Code).toEqual('ResourceNotFoundException');
+    }
+  });
+
+  it('should label errorType with "abort" when non-allowed error occurs', async () => {
+    const DDBReply: AwsError = {
+      Type: 'Sender',
+      Code: 'RequestLimitExceeded',
+      name: 'RequestLimitExceeded',
+      $fault: 'client',
+      $metadata: {
+        httpStatusCode: 400,
+        requestId: 'df840ab9-e68b-5c0e-b4a0-5094f2dfaee8',
+        attempts: 1,
+        totalRetryDelay: 0,
+      },
+    };
+    const adapter = new DynamoDBAdapter(Logger);
+    const mockEvent = {
+      event: 'loading',
+      timestamp: 0,
+      playhead: 0,
+      duration: 0,
+      host: 'mock.tenant.mock',
+    };
+    ddbMock.on(PutItemCommand).rejects(DDBReply);
+    try {
+      const result = await adapter.putItem({
+        tableName: 'table_1',
+        data: mockEvent,
+      });
+    } catch (err) {
+      expect(err.errorType).toEqual(ErrorType.ABORT);
+      expect(err.error.Code).toEqual('RequestLimitExceeded');
+    }
+  });
+
+  it('should get item in database', async () => {
+    const DDBReply: GetItemCommandOutput = {
+      $metadata: {},
+      Item: { eventId: { S: '123-123-123-123' } },
+    };
+    const adapter = new DynamoDBAdapter(Logger);
+    const mockEventId = '123-123-123-123';
+    ddbMock.on(GetItemCommand).resolves(DDBReply);
+    const result = await adapter.getItem({
+      tableName: 'table_1',
+      eventId: mockEventId,
+    });
+    expect(result).toEqual({
+      $metadata: {},
+      Item: { eventId: { S: '123-123-123-123' } },
+    });
+  });
+
+  it('should delete item in database', async () => {
+    const DDBReply: DeleteItemCommandOutput = {
+      $metadata: {},
+    };
+    const adapter = new DynamoDBAdapter(Logger);
+    const mockEventId = '123-123-123-123';
+    ddbMock.on(DeleteItemCommand).resolves(DDBReply);
+    const result = await adapter.deleteItem({
+      tableName: 'table_1',
+      eventId: mockEventId,
+    });
+    expect(result).toEqual({
+      $metadata: {},
+    });
+  });
+});
--- types/interfaces.ts
+++ types/interfaces.ts
@@ -7,10 +7,40 @@ export interface EventValidator {
   validateEventList(eventList: Array<Object>): any;
 }
 
+export enum ErrorType {
+  'ABORT' = 0,
+  'CONTINUE' = 1,
+}
+export interface IHandleErrorOutput {
+  errorType: ErrorType;
+  error: Object;
+}
+
+export interface IDDBPutItemInput {
+  tableName: string;
+  data: Object;
+}
+export interface IDDBGetItemInput {
+  tableName: string;
+  eventId: string;
+}
+
 export abstract class AbstractQueueAdapter {
   logger: winston.Logger;
   client: any;
   abstract pushToQueue(body: Object): Promise<Object>;
   abstract pullFromQueue(body: Object): Promise<Object>;
   abstract removeFromQueue(body: Object): Promise<Object>;
+}
+export abstract class AbstractDBAdapter {
+  logger: winston.Logger;
+  dbClient: any;
+
+  abstract getTableNames(): Promise<string[]>;
+  abstract createTable(name: string): Promise<any>;
+  abstract putItem(params: Object): Promise<any>;
+  abstract getItem(params: Object): Promise<any>;
+  abstract deleteItem(params: Object): Promise<any>;
+  abstract getItemsBySession(params: Object): Promise<any>;
+  abstract handleError(error: any): IHandleErrorOutput;
 }
