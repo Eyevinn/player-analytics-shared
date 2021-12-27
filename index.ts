c0ce684721c98705ef23df1fd504a67a61f3a116
Erik <erik.johansson87@gmail.com>
2021-12-27 13:29:34 +0100

    fix: export the redis queue adapter

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 export * from './adapters/queue/SqsQueueAdapter';
 export * from './adapters/queue/BeanstalkdAdapter';
+export * from './adapters/queue/RedisAdapter';
+
 export * from './adapters/db/DynamoDBAdapter';
