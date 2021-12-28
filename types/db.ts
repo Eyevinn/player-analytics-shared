a93e17ece13fcaa5eedf00787f577b901a282421
Erik <erik.johansson87@gmail.com>
2021-12-28 08:54:47 +0100

    fixup! fix: adapter method interface typing

---
+++ types/db.ts
@@ -0,0 +1,40 @@
+import winston from 'winston';
+
+export enum ErrorType {
+  'ABORT' = 0,
+  'CONTINUE' = 1,
+}
+
+export interface IHandleErrorOutput {
+  errorType: ErrorType;
+  error: Object;
+}
+
+export interface IPutItemInput {
+  tableName: string;
+  data: Object;
+}
+
+export interface IGetItems {
+  sessionId: string;
+  tableName: string;
+}
+
+export interface IGetItemInput {
+  tableName: string;
+  sessionId: string;
+  timestamp: number;
+}
+
+export abstract class AbstractDBAdapter {
+  logger: winston.Logger;
+  dbClient: any;
+
+  abstract getTableNames(): Promise<string[]>;
+  abstract createTable(name: string): Promise<any>;
+  abstract putItem(params: IPutItemInput): Promise<any>;
+  abstract getItem(params: IGetItemInput): Promise<any>;
+  abstract deleteItem(params: IGetItemInput): Promise<any>;
+  abstract getItemsBySession(params: IGetItems): Promise<any>;
+  abstract handleError(error: any): IHandleErrorOutput;
+}
